0faabac6e4b836b8f4ee4b92b06d1a4ef9179c26
Tanusree <tanusreekolkata2013@gmail.com>
2020-01-28 14:27:46 +0530

    start node 2

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
+
+const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
