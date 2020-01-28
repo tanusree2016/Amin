bb7cdb07c5a4421ee07438b3d64c3f7e14954f3f
Tanusree <tanusreekolkata2013@gmail.com>
2020-01-28 15:48:12 +0530

    env

--- app.js
+++ app.js
@@ -27,5 +27,6 @@ const PORT = process.env.PORT || 5000;
 //const PORT = 5000;
 
 app.listen(PORT, () => {
+    console.log(process.env.DATABASE_URL);
     console.log(`Server is running on PORT ${PORT}`);
 });
