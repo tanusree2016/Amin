0e5d65470355b55d3f419fa72c0f22da41d6da64
Tanusree <tanusreekolkata2013@gmail.com>
2020-01-28 15:50:53 +0530

    envissue

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const Sequelize = require("sequelize");
 const db = {}
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize('amin_vvs', 'aminVvsDb', '%MgEBkq3j;}~', {
+    host: 'arystonwebsolution.com',
     dialect: 'mysql',
     operatorsAliases: false,
     freezeTableName: true, 
