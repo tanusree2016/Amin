7ea0a70dadfec3de499990a55b27c09cdd554900
Tanusree <tanusreekolkata2013@gmail.com>
2020-01-29 13:11:31 +0530

    test 2

--- app.js
+++ app.js
@@ -23,13 +23,13 @@ app.get('/', function(req, res) {
 //     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 // });
 
-//const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-const PORT = 5000;
+//const PORT = 5000;
 
-// app.listen(PORT, ' 192.168.1.132', () => {
-//    // console.log(process.env.DATABASE_URL);
-//     console.log(`Server is running on PORT ${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log('dd'+process.env.DATABASE_URL);
+    console.log(`Server is running on PORT ${PORT}`);
+});
 
-app.listen(5000, "192.168.1.132");
+//app.listen(5000, "192.168.1.132");
